798acf625b3c8c382a32b0722297b9050da00555
Tom <tom.odell@strident.co.uk>
2016-10-14 18:07:05 +0100

    initial commit, up to 09.1

---
+++ SuperAdventure/SuperAdventure.cs
@@ -0,0 +1,44 @@
+﻿using Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// <summary>
+/// Coded from http://scottlilly.com/learn-c-by-building-a-simple-rpg-index/
+/// currently up to 09.1
+/// </summary>
+namespace SuperAdventure
+{
+    public partial class SuperAdventure : Form
+    {
+        private Player _player;
+        public SuperAdventure()
+        {
+            InitializeComponent();
+
+            SetUpNewPlayer();
+
+            SetUpFields();
+
+        }
+
+        private void SetUpNewPlayer()
+        {
+            _player = new Player(10, 10, 20, 0, 1);
+        }
+
+        private void SetUpFields()
+        {
+            this.lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            this.lblGold.Text = _player.Gold.ToString();
+            this.lblExperience.Text = _player.ExperiencePoints.ToString();
+            this.lblLevel.Text = _player.Level.ToString();
+        }
+    }
+}
